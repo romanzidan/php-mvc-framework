7b7f3a115c96e63396a91462a307022336576457
Roman Zidan Ramadhan <romanzidanramadhan@gmail.com>
2021-08-29 09:45:21 +0800

    First init

---
+++ controllers/SiteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Controller;
+use app\core\Request;
+
+class SiteController extends Controller
+{
+    public function home(){
+        $params = [
+            'name' => 'Kyze Luci Garendra'
+        ];
+        return $this->render('home', $params);
+    }
+
+    public function contact()
+    {
+        return $this->render('contact');
+    }
+
+    public function handleContact(Request $request)
+    {
+        $body = $request->getBody();
+        echo '<pre>';
+        var_dump($body);
+        echo '</pre>';
+        exit;
+        return 'Handling submitted data contact';
+    }
+}
+?>
